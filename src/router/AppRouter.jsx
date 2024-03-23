59095b4df52abb08798078c0d7cbfa5d8fe54d32
alejopua <alejopua@gmail.com>
2024-03-23 12:15:53 -0500

    Add authentication routes and update AppRouter

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+
+      {/* Login and Register */}
+      <Route path="path" element={<path />} />
+
+      {/* Journal app */}
+      <Route path="path" element={<path />} />
+
+    </Routes>
+  )
+}
