37fd71b8b99a49c47d57ce2daec829a95f7f9133
alejopua <alejopua@gmail.com>
2024-03-26 11:36:40 -0500

    Add JournalLayout component to handle the layout of the journal page

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import { Box } from '@mui/system'
+import { NavBar } from '../components';
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display:'flex' }}>
+      <NavBar drawerWidth={ drawerWidth } />
+
+      {/* Sidebar drawerWidth*/}
+
+      <Box component='main' sx={{ flexGrow:1, p:3 }}>
+        {/* ToolBar */}
+
+        {children}
+      </Box>
+
+
+    </Box>
+  )
+}
+
+JournalLayout.propTypes = {
+  children: PropTypes.element.isRequired
+}
