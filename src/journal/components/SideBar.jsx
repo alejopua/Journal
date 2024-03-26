936817f6d09744ef21cfdedfc6e5b1763b36f011
alejopua <alejopua@gmail.com>
2024-03-26 13:50:46 -0500

    Add responsive layout to SideBar component

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,6 +1,51 @@
+import PropTypes from 'prop-types'
+import { TurnedInNot } from '@mui/icons-material'
+import { ListItemText, Grid, ListItemIcon, Divider, Toolbar, List, ListItem, ListItemButton, Typography, Box, Drawer } from '@mui/material'
 
-export const SideBar = () => {
+export const SideBar = ({ drawerWidth = 240 }) => {
   return (
-    <div>SideBar</div>
+    <Box
+      component='nav'
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { xs: 'block' },
+          '.MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+
+        }}  
+      >
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div'>Alejandro Ramirez</Typography>
+        </Toolbar>
+        <Divider />
+
+        <List>
+          {
+            ['Enero', 'Febrero', 'Marzo', 'Abril'].map( text => (
+              <ListItem key={ text } disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <TurnedInNot />
+                  </ListItemIcon>
+                  <Grid container>
+                    <ListItemText primary={ text } />
+                    <ListItemText secondary={ 'Laboris reprehenderit mollit quis.' }/>
+                  </Grid>
+                </ListItemButton>
+              </ListItem>
+            ))
+          }
+        </List>
+        
+      </Drawer>
+    </Box>
+
   )
+}
+
+SideBar.propTypes = {
+  drawerWidth: PropTypes.number
 }
