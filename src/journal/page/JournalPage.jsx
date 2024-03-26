ab5bf5e103cae85e7405bb29aec78c3c0f9b2f2c
alejopua <alejopua@gmail.com>
2024-03-26 11:36:47 -0500

    Add JournalLayout and update JournalPage content

--- src/journal/page/JournalPage.jsx
+++ src/journal/page/JournalPage.jsx
@@ -1,8 +1,15 @@
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <Typography variant='h1'>JournalPage</Typography>
-    // <Typography component='h1'>JournalPage</Typography>
+    <JournalLayout>
+      <Typography variant='p'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nisi praesentium neque quo, laborum saepe eos qui suscipit facere? Nostrum doloribus fugiat fuga ut sed, perferendis porro enim necessitatibus impedit!</Typography>
+      {/* <Typography component='h1'>JournalPage</Typography> */}
+      
+      {/* NothinSelected */}
+      {/* NoteView */}
+
+    </JournalLayout>
   )
 }
