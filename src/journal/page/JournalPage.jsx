eaa0d04d5d4b0a3a0e48fe457237fec979a8d926
alejopua <alejopua@gmail.com>
2024-03-26 23:46:25 -0500

    Refactor JournalPage component to use NothingSelectedViews

--- src/journal/page/JournalPage.jsx
+++ src/journal/page/JournalPage.jsx
@@ -1,15 +1,10 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedViews } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography variant='p'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque nisi praesentium neque quo, laborum saepe eos qui suscipit facere? Nostrum doloribus fugiat fuga ut sed, perferendis porro enim necessitatibus impedit!</Typography>
-      {/* <Typography component='h1'>JournalPage</Typography> */}
-      
-      {/* NothinSelected */}
-      {/* NoteView */}
-
+      <NothingSelectedViews />
     </JournalLayout>
   )
 }
