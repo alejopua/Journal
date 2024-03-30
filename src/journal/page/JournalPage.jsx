0e41029b872ecd926234b57a98808c9767e5f619
alejopua <alejopua@gmail.com>
2024-03-29 23:15:54 -0500

    Add "Add" button to JournalPage

--- src/journal/page/JournalPage.jsx
+++ src/journal/page/JournalPage.jsx
@@ -1,11 +1,26 @@
+import { AddOutlined } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView } from "../views"
+import { NoteView, NothingSelectedViews } from "../views"
+import IconButton from '@mui/material/IconButton'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedViews /> */}
-      <NoteView />
+      <NothingSelectedViews />
+      {/* <NoteView /> */}
+      <IconButton 
+        size="large" 
+        sx={{ 
+            color: 'white',
+            backgroundColor: 'error.main', 
+            ':hover': { backgroundColor: 'error.main', opacity: '0.8'}, 
+            position: 'fixed', 
+            right: 34, 
+            bottom: 32
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
     </JournalLayout>
   )
 }
