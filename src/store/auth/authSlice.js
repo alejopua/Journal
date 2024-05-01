3e736bccf91510186491762e3a84f36a06dae238
alejopua <alejopua@gmail.com>
2024-05-01 01:35:02 -0500

    Update authSlice to include login, logout, and checkingCredentials actions

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,17 +1,23 @@
+import { Logout } from "@mui/icons-material";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  counter: 10,
+  status: "checking",
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    increment: (state /* action */) => {
-      state.counter += 1;
-    },
+    login: (state, action) => {},
+    logout: (state, payload) => {},
+    checkingCredentials: (state) => {},
   },
 });
 
-export const { increment } = authSlice.actions;
+export const { login, logout, checkingCredentials } = authSlice.actions;
