dc9ee751869e34dc0eda42da8aa9753d1b7a53ec
alejopua <alejopua@gmail.com>
2024-05-01 01:19:51 -0500

    Update main.jsx to use Redux store and Provider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './styles.css'
-import { JournalApp } from './JournalApp'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles.css";
+import { JournalApp } from "./JournalApp";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
