5af231807f686bbf1c5d65dd9095324425a8c1e2
alejopua <alejopua@gmail.com>
2024-05-01 00:53:53 -0500

    Refactor AppTheme component to use consistent import formatting

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -1,13 +1,13 @@
-import { ThemeProvider } from "@emotion/react"
-import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 import { preferTheme } from "./preferTheme";
 
 export const AppTheme = ({ children }) => {
   return (
-    <ThemeProvider theme={ preferTheme }>
+    <ThemeProvider theme={preferTheme}>
       <CssBaseline />
 
       {children}
     </ThemeProvider>
-  )
-}
+  );
+};
