5addcf0f387f077cea643937518b9e19d747f4f8
alejopua <alejopua@gmail.com>
2024-05-06 13:03:04 -0500

    Update Firebase configuration and add authentication functionality

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,3 +17,5 @@ const firebaseConfig = {
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
