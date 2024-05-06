5f60c006b4b13850e60fc50e7aab674ccbff42af
alejopua <alejopua@gmail.com>
2024-05-06 12:58:08 -0500

    Update Firebase configuration in src/firebase/config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_SOME_FIREBASE_API_KEY,
-  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID,
+  authDomain: import.meta.env.VITE_SOME_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_SOME_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_SOME_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_SOME_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_SOME_FIREBASE_APP_ID,
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
