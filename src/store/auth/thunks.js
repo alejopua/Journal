398f22bb4e42a9bb3200805b0f5a3122fa979158
alejopua <alejopua@gmail.com>
2024-05-07 09:14:48 -0500

    Add Google sign-in functionality to authentication thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./authSlice";
 
 export const checkingAuthentication = (email, password) => {
@@ -10,5 +11,7 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSingIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+    const result = await singInWithGoogle();
+    console.log({ result });
   };
 };
