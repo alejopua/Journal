3f937f684a7a6f556becb88a64127079508c6bb5
alejopua <alejopua@gmail.com>
2024-05-13 22:56:58 -0500

    refactor: Update authSlice to handle Google sign-in errors and improve authentication functionality

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -1,4 +1,3 @@
-import { Logout } from "@mui/icons-material";
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
@@ -14,8 +13,22 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    login: (state, action) => {},
-    logout: (state, payload) => {},
+    login: (state, { payload }) => {
+      state.status = "authenticated";
+      state.uid = payload.uid;
+      state.email = payload.email;
+      state.displayName = payload.displayName;
+      state.photoURL = payload.photoURL;
+      state.errorMessage = null;
+    },
+    logout: (state, { payload }) => {
+      state.status = "not-authenticated";
+      state.uid = null;
+      state.email = null;
+      state.displayName = null;
+      state.photoURL = null;
+      state.errorMessage = payload.errorMessage;
+    },
     checkingCredentials: (state) => {
       state.status = "checking";
     },
